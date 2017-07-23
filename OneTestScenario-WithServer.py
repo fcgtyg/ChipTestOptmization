93bf97572ad69b122750467b853b0f39c13311b3
Fatih Çağatay Gülmez <gulmez.fatihcagatay@yahoo.com.tr>
2017-07-24 00:44:08 +0300

    Cleaning the code

--- OneTestScenario-WithServer.py
+++ OneTestScenario-WithServer.py
@@ -1,26 +1,29 @@
 import threading
-from sys import exit
+
+
 class SparkThread(threading.Thread):
+
     def __init__(self):
         threading.Thread.__init__(self)
         pass
+
     def run(self):
         from OneTestScenario import SparkApp
         SparkApp()
 
 
 class ServerThread(threading.Thread):
+
     def __init__(self):
         threading.Thread.__init__(self)
         pass
+
     def run(self):
         import RandomNumberServer
         RandomNumberServer.start()
 
 threadSpark = SparkThread()
 threadServer = ServerThread()
-
-print "Hello"
 
 threadSpark.start()
 threadServer.start()
